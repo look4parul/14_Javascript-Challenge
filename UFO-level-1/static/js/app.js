1a9cc6d00cd0a4d62a30024a455ea52ef11a75e8
Parul <parul.g27@gmail.com>
2020-03-06 03:06:42 -0600

    Adding code to append table and add new rows of data for each UFO sighting

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,40 @@
-// from data.js
-const tableData = data;
+let tbody = d3.select("thead");
 
-// YOUR CODE HERE!
+data.forEach(e => {
+    let row = tbody.append("tr", true)
+    row.append("th").text(e.datetime)
+    row.append("th").text(e.city)
+    row.append("th").text(e.state)
+    row.append("th").text(e.country)
+    row.append("th").text(e.shape)
+    row.append("th").text(e.durationMinutes)
+    row.append("th").text(e.comments)
+
+});
+
+// Select the button
+let button = d3.select("#filter-btn").on("click", function() {
+    // Select the input date 
+    let inputDate = d3.select("#datetime").property("value");
+    // Display the input date
+    console.log("Input date is: ", inputDate);
+        
+    // Filter Data with datetime equal to input value
+    let filteredData = data.filter(e => e.datetime === inputDate);
+    //  Display the filtered data for the entered date on the web page
+    console.log("filtered data is: " , filteredData);
+    
+    filteredData.forEach(e => {
+    // Append one table row for each UFO Sighting object for the date entered when button is clicked
+    let row = d3.select("thead").append("tr", true)
+    row.append("th").text(e.datetime)
+    console.log(" Date value at column is: ", row.append("th").text(e.datetime))
+    row.append("th").text(e.city)
+    row.append("th").text(e.state)
+    row.append("th").text(e.country)
+    row.append("th").text(e.shape)
+    row.append("th").text(e.durationMinutes)
+    row.append("th").text(e.comments)
+    });
+});
+    
