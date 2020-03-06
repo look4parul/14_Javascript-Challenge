5e195ef4d62cfcf9c64671ca46d3becc05c3c920
Parul <parul.g27@gmail.com>
2020-03-06 14:17:15 -0600

    format code, part-1 completed

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,5 +1,7 @@
+// Select the table part from the index.html
 let tbody = d3.select("thead");
 
+// Append the data from data.js file to the web page
 data.forEach(e => {
     let row = tbody.append("tr", true)
     row.append("th").text(e.datetime)
@@ -14,16 +16,17 @@ data.forEach(e => {
 
 // Select the button
 let button = d3.select("#filter-btn").on("click", function() {
-
+    // Hide the table data on the web page
     tbody.html("");
     // Select the input date 
     let inputDate = d3.select("#datetime").property("value");
     // Display the input date
     console.log("Input date is: ", inputDate);
 
-    //  Check for blank input date
+    //  Error handling for blank input date
     if (inputDate == '') {
-        alert("Please enter a date value in the search filter box!");
+        alert("Please enter a date value in the search filter box!!!!!");
+        // Code to add table header
         let tbody = d3.select("thead");
         let row = tbody.append("tr", true)
         row.append("th").text("Date")
@@ -33,11 +36,10 @@ let button = d3.select("#filter-btn").on("click", function() {
         row.append("th").text("Shape")
         row.append("th").text("Duration")
         row.append("th").text("Comments")
-
+        
+        // Append the table with data from data.js file 
         data.forEach(e => {
             let row = tbody.append("tr", true)
-            // Date = row.append("th", true)
-            // row.append("th").text("Date")
             row.append("th").text(e.datetime)
             row.append("th").text(e.city)
             row.append("th").text(e.state)
@@ -47,16 +49,19 @@ let button = d3.select("#filter-btn").on("click", function() {
             row.append("th").text(e.comments)
 
         });
-    }   
+    }  
+    // Code to append data for the entered data in filter box 
     else{
         // Filter Data with datetime equal to input value
         let filteredData = data.filter(e => e.datetime === inputDate);
         //  Display the filtered data for the entered date on the web page
         console.log("filtered data is: " , filteredData);
+        // Error handling for invalid date or date where no UFO records are present in data.js file
         if (filteredData.length === 0){
             alert("No UFO sightings found for the entered date")}
         else {    
-            let row = tbody.append("tr", true)
+        // Code to add table header
+        let row = tbody.append("tr", true)
                 row.append("th").text("Date")
                 row.append("th").text("City")
                 row.append("th").text("State")
@@ -66,13 +71,9 @@ let button = d3.select("#filter-btn").on("click", function() {
                 row.append("th").text("Comments")
 
             filteredData.forEach(e => {
-                // console.log("row value is: ", row1);
-
                 // Append one table row for each UFO Sighting object for the date entered when button is clicked
                 let row = d3.select("thead").append("tr", true)
-                // console.log("row: ", row);
                 row.append("th").text(e.datetime)
-                // console.log(" Date value at column is: ", row.append("th").text(e.datetime))
                 row.append("th").text(e.city)
                 row.append("th").text(e.state)
                 row.append("th").text(e.country)
